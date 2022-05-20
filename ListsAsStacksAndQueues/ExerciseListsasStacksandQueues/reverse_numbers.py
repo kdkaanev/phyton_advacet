8e98ef587bd21450190ed6ce3e532dffa34bcfcf
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-05-20 07:49:37 +0300

    next exercist

--- ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/reverse_numbers.py
+++ ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/reverse_numbers.py
@@ -1,9 +1,13 @@
+'''
 integers = input()
 stack = integers.split(' ')
 revers_number = []
 while stack:
     revers_number.append(stack.pop())
 print(' '.join(revers_number))
+'''
 
-
-
+numbers = input().split(' ')
+while numbers:
+    revers_number = numbers.pop()
+    print(revers_number, end=' ')
