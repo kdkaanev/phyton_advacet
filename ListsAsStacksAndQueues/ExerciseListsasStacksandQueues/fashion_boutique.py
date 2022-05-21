124af3a6d4a8f12adf939dc414214f946a8c3145
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-05-21 07:04:31 +0300

    saturdey morning

---
+++ ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/fashion_boutique.py
@@ -0,0 +1,15 @@
+clothes_box = input()
+rack_capacity = int(input())
+clothes_stack = [int(ch) for ch in clothes_box if ch != ' ']
+rack_number = 1
+rack = []
+
+while clothes_stack:
+    clothes = clothes_stack.pop()
+    rack.append(clothes)
+    if rack_capacity < sum(rack):
+        rack = []
+        rack.append(clothes)
+        rack_number += 1
+print(rack_number)
+
