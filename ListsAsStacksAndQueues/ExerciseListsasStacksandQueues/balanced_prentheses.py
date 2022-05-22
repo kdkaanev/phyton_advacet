d8745f5028f1b276ff5089d65fb4136b692ccda4
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-05-22 12:13:27 +0300

    lst revision

--- ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/balanced_prentheses.py
+++ ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/balanced_prentheses.py
@@ -9,14 +9,17 @@ pairs = {
 for ch in bracket_string:
     if ch in '({[':
         bracket_stack.append(ch)
+    elif not bracket_stack:
+        balanced = False
     else:
         last_opening_bracket = bracket_stack.pop()
         if pairs[last_opening_bracket] != ch:
             balanced = False
-            break
-if balanced:
-    print('YES')
-else:
+    if not balanced:
+        break
+if  not balanced or bracket_stack:
     print('NO')
+else:
+    print('YES')
 
 
