056c183f2f249fc761268889255fa6038afde097
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-05-22 20:22:28 +0300

    final

---
+++ ListsAsStacksAndQueues/ExerciseListsasStacksandQueues/fast_food.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+food_quantity = int(input())
+orders = input().split(' ')
+queue = deque([int(o) for o in orders])
+max_order = max(queue)
+print(max_order)
+stop = False
+while queue:
+    order = queue.popleft()
+    if order <= food_quantity:
+        food_quantity -= order
+    else:
+        queue.appendleft(order)
+        break
+
+if len(queue) == 0:
+    print('Orders complete')
+else:
+    queue_string = [str(x) for x in queue]
+    left = ' '.join(queue_string)
+    print(f"Orders left: {left}")
