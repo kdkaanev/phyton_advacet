ca311e6951765cb854fffbb26b6ee58f416ab0c4
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-05-29 11:59:41 +0300

    vercion.2

--- Tuples and Sets/ExerciseTuplesandSets/periodic_table.py
+++ Tuples and Sets/ExerciseTuplesandSets/periodic_table.py
@@ -1,7 +1,14 @@
 n = int(input())
 chemical_elements = set()
+'''
 for _ in range(n):
     chemical_compound  = [co for co in input().split(' ')]
     for element in chemical_compound:
         chemical_elements.add(element)
+[print(element) for element in chemical_elements]
+'''
+
+for _ in range(n):
+    compound = set(element for element in input().split(' '))
+    chemical_elements = chemical_elements.union(compound)
 [print(element) for element in chemical_elements]
