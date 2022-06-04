4ef9cf66db4e5715b606c87250c25e51431d98c9
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-04 19:23:18 +0300

    final

---
+++ Tuples and Sets/num3.py
@@ -0,0 +1,3 @@
+sequence_first = set(n for n in input().split('-'))
+sequence_second = set(n for n in input().split('-'))
+print()
--- lsq_exercist/milkshakes.py
+++ lsq_exercist/milkshakes.py
@@ -20,19 +20,27 @@ while True:
     else:
         current_cup = milk_cup.popleft()
         current_choko = chocolates.pop()
-        if current_choko != current_cup and current_choko > 5:
+        if current_choko <= 0 and current_cup <= 0:
+            continue
+        if current_choko <= 0:
+            milk_cup.appendleft(current_cup)
+            continue
+        if current_cup <= 0:
+            chocolates.append(current_choko)
+            continue
+        if current_choko != current_cup:
+
             chocolates.append(current_choko - 5)
         elif current_cup == current_choko:
             milkshake += 1
-        else:
-            milk_cup.appendleft(current_cup)
+
 if is_all:
     print("Great! You made all the chocolate milkshakes needed!")
 else:
     print("Not enough milkshakes.")
 if chocolates:
-   chocolates = [str(choko) for choko in chocolates]
-   print(f"Chocolate: {', '.join(chocolates)}")
+    chocolates = [str(choko) for choko in chocolates]
+    print(f"Chocolate: {', '.join(chocolates)}")
 else:
     print("Chocolate: empty")
 if milk_cup:
@@ -40,4 +48,3 @@ if milk_cup:
     print(f"Milk: {', '.join(milk_cup)}")
 else:
     print("Milk: empty")
-
