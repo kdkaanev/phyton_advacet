ba7b027596f99741efd80185aa6173a7404c4bb5
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-04 14:52:29 +0300

    .75

---
+++ lsq_exercist/milkshakes.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+'''
+def stack_top(choko):
+    result = choko.pop()
+    choko.append(result)
+    return result
+'''
+
+chocolates = [int(x) for x in input().split(', ')]
+milk_cup = deque([int(x) for x in input().split(', ')])
+milkshake = 0
+is_all = False
+
+while True:
+    if milkshake == 5:
+        is_all = True
+    if is_all or not chocolates or not milk_cup:
+        break
+    else:
+        current_cup = milk_cup.popleft()
+        current_choko = chocolates.pop()
+        if current_choko != current_cup and current_choko > 5:
+            chocolates.append(current_choko - 5)
+        elif current_cup == current_choko:
+            milkshake += 1
+        else:
+            milk_cup.appendleft(current_cup)
+if is_all:
+    print("Great! You made all the chocolate milkshakes needed!")
+else:
+    print("Not enough milkshakes.")
+if chocolates:
+   chocolates = [str(choko) for choko in chocolates]
+   print(f"Chocolate: {', '.join(chocolates)}")
+else:
+    print("Chocolate: empty")
+if milk_cup:
+    milk_cup = [(str(milk)) for milk in milk_cup]
+    print(f"Milk: {', '.join(milk_cup)}")
+else:
+    print("Milk: empty")
+
