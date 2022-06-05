1619bd0011d376326a420d7811f8f399559d0c85
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-05 15:27:48 +0300

    final revision

--- lsq_exercist/paint_colors.py
+++ lsq_exercist/paint_colors.py
@@ -1,21 +1,43 @@
-text = 're ple blu pop e pur d'
+text = input()
 colors = text.split()
-main_colors = ["red", "yellow", "blue"]
-secondary_colors = ["orange", "purple", "green"]
+main_colors = {"red", "yellow", "blue"}
+secondary_colors = {"orange", "purple", "green"}
 output = []
+mixed_color = {
+    'orange': ['red', 'yellow'],
+    'purple': ['red', 'blue'],
+    'green': ['yellow', 'blue']
+
+}
 while colors:
     first_string = colors.pop(0)
-    if len(colors) > 1:
-        second_string = colors.pop(-1)
-        substring = first_string + second_string
-    else: substring =first_string
+    second_string = colors.pop(-1) if colors else ''
+    substring = first_string + second_string
     if substring in main_colors or substring in secondary_colors:
         output.append(substring)
-    else:
-        first_string = first_string[:-1]
-        second_string = second_string[:-1]
-        if first_string :
-            colors.insert(len(colors)//2, first_string)
-        if second_string:
-            colors.insert(len(colors)//2,second_string)
-print()
+        continue
+    substring = second_string + first_string
+    if substring in main_colors or substring in secondary_colors:
+        output.append(substring)
+        continue
+
+    first_string = first_string[:-1]
+    second_string = second_string[:-1]
+    if first_string:
+        colors.insert(len(colors) // 2, first_string)
+    if second_string:
+        colors.insert(len(colors) // 2, second_string)
+result = []
+
+for color in output:
+    if color in main_colors:
+        result.append(color)
+        continue
+    is_colected = True
+    for help_color in mixed_color[color]:
+        if help_color not in output:
+            is_colected =False
+            break
+    if is_colected:
+        result.append(color)
+print(result)
