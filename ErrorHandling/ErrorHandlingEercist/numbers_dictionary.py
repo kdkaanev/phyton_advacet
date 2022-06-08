e99335ab48fd12247fec8794f498236467372ff7
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-08 18:31:00 +0300

    0.2

--- ErrorHandling/ErrorHandlingEercist/numbers_dictionary.py
+++ ErrorHandling/ErrorHandlingEercist/numbers_dictionary.py
@@ -1,22 +1,18 @@
 import sys
 from io import StringIO
+
 test_input1 = '''one
-1
 two
-2
 Search
-one
 Remove
-two
 End
 
+
 '''
-test_input2 = '''2, 4
-10, 11, 12, 13
-14, 15, 16, 17
-'''
+
 sys.stdin = StringIO(test_input1)
-#sys.stdin = StringIO(test_input2)
+
+
 
 numbers_dictionary = {}
 
@@ -24,8 +20,12 @@ line = input()
 
 while line != "Search":
     number_as_string = line
-    number = int(input())
-    numbers_dictionary[number_as_string] = number
+    try:
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print('The variable number must be an integer')
+
     line = input()
 
 line = input()
