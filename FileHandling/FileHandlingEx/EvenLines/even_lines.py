8d7e1c8db2e3b1db5e83b1a98abaf720b643364b
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-11 15:18:32 +0300

    new version

--- FileHandling/FileHandlingEx/EvenLines/even_lines.py
+++ FileHandling/FileHandlingEx/EvenLines/even_lines.py
@@ -1,17 +1,11 @@
-symbols = {"-", ",", ".", "!", "?"}
+symbols = ["-", ",", ".", "!", "?"]
 even_lines = []
 reading_file = open('text.txt', 'r')
 
 for index, lines in enumerate(reading_file):
-
-    for symbol in symbols:
-        if symbol in lines:
-            lines = lines.replace(symbol, '@')
     if index % 2 == 0:
-        even_lines.append(lines)
-for lines in even_lines:
-    s = lines.split()[::-1]
-    l = []
-    for i in s:
-        l.append(i)
-    print(" ".join(l))
+        line = ' '.join(lines.strip().split()[::-1])
+        for symbol in symbols:
+            if symbol in line:
+                line = line.replace(symbol, '@')
+        print(line)
