44cc9dadcd8db46d514be145f7933f38caede389
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-12 10:25:23 +0300

    add file

--- FileHandling/FileHandlingEx/LineNumbers/line_numbers.py
+++ FileHandling/FileHandlingEx/LineNumbers/line_numbers.py
@@ -11,7 +11,8 @@ def number_of_chars(line):
     return f'({letter_count})({len(punctuation_marks)})'
 
 
-with open('./text.txt', 'r') as text:
+with open('./text.txt', 'r') as text, open('./output.txt', 'w') as output:
     for index, line in enumerate(text):
-        print(f'Line {index + 1}: {line.strip()}')
-        print(number_of_chars(line))
+        output.write(f'Line {index + 1}: {line.strip()} {number_of_chars(line)}\n')
+
+
