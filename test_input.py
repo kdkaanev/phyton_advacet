97e61a1fa425b8b901e05f8ac60a0e59817bf96b
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-14 20:20:28 +0300

    0.1

--- test_input.py
+++ test_input.py
@@ -1,19 +1,27 @@
-import sys
-from io import StringIO
-
-test_input1 = '''5 4
-A A B D
-A A B B
-I J B B
-C C C G
-C C K P
+def check(swap, r, c, val):
+    return 'swap' in swap and len(swap) == 5 and all(x >= 0 for x in val) and int(swap[1]) <= r and int(
+        swap[3]) <= r and int(swap[2]) <= c and int(swap[4]) <= c
 
 
+rows, columns = [int(x) for x in input().split()]
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
-'''
-# test_input2 = '''2, 4
-# 10, 11, 12, 13
-# 14, 15, 16, 17
+command = input()
+while command != 'END':
+    swap = command.split()
+    r1, c1, r2, c2 = int(swap[1]), int(swap[2]), int(swap[3]), int(swap[4])
+    value = [r1, c1, r2, c2]
+    if check(swap, rows, columns, value):
 
-sys.stdin = StringIO(test_input1)
-# sys.stdin = StringIO(test_input2)
+        a = matrix[r1]
+        b = matrix[r2]
+        x = a.pop(c1)
+        a.insert(c1, b[c2])
+        b.pop(c2)
+        b.insert(c2, x)
+        for element in matrix:
+            print(' '.join([str(x) for x in element]))
+    else:
+        print('Invalid input!')
+
+    command = input()
