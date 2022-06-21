e16782b5cddcd0349aaefa87d2957edd34853bb8
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-21 19:25:18 +0300

    first revision

--- FunctionAdvancet/ExerciseFunctionsAdvanced/even_or_odd.py
+++ FunctionAdvancet/ExerciseFunctionsAdvanced/even_or_odd.py
@@ -1,5 +1,12 @@
 def even_odd(*args):
-    num = [x for _, x in args]
+    result = []
+    command = args[-1]
+    check = 0 if command == 'even' else 1
+    for i in range(len(args) - 1):
+        if args[i] % 2 == check:
+            result.append(args[i])
+
+    return result
 
 
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))
