2654b26118652a5d57f7b354ae408f9e1eebe0aa
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-22 19:10:58 +0300

    final

--- FunctionAdvancet/ExerciseFunctionsAdvanced/age_assignment.py
+++ FunctionAdvancet/ExerciseFunctionsAdvanced/age_assignment.py
@@ -1,13 +1,14 @@
 def age_assignment(*args, **kwargs):
     result = {}
-    names = [n for n in args]
-    dict = {key:value for key,value in kwargs.items()}
-    for letter, age in dict.items():
-        for name in names:
-            if name[0] == letter:
-                result[name] = age
-    sorted_result = [f'{name} is {age} years old' for name, age in sorted(result.items())]
+    for name in args:
+        first_letter = name[0]
+        age = kwargs[first_letter]
+        result[name] = age
+    sorted_result = [f'{key} is {value} years old.' for key, value in sorted(result.items(), key=lambda x: x[0])]
     return '\n'.join(sorted_result)
 
-
 print(age_assignment("Peter", "George", G=26, P=19))
+
+
+
+
