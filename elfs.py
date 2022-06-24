0d6cd0f77e4366691c85520387ad3a75161696d5
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-06-24 07:25:03 +0300

    demo

--- elfs.py
+++ elfs.py
@@ -4,21 +4,37 @@ elfs_energy = deque(int(x) for x in input().split(' '))
 materials = [int(x) for x in input().split()]
 total_energy = 0
 total_toys = 0
-count =0
+day = 0
 while True:
-    if  not elfs_energy or materials:
+    if not elfs_energy or not materials:
         break
+    day += 1
     elf = elfs_energy.popleft()
-    box = materials.pop()
-    if count % 3 == 0:
-       
-       if elf <= box * 2:
-           elfs_energy.append(elf * 2)
-           materials.append(box)
-       else:
-          total_toys += 2
-          total_energy += elf - box
-          elfs_energy.append(elf + 1)
-    if count % 5 == 0:
-        
-          
+    toy = materials.pop()
+    if elf < 5:
+        continue
+    if elf < toy:
+        elfs_energy.append(elf * 2)
+        materials.append(toy)
+    elif day % 3 == 0:
+        if elf < toy * 2:
+            elfs_energy.append(elf * 2)
+            materials.append(toy)
+
+        elif day % 5 != 0:
+            total_energy += toy * 2
+            total_toys += 2
+            elfs_energy.append(elf - toy + 1)
+        elif day % 5 == 0:
+            elfs_energy.append(elf - toy)
+            total_energy += toy * 2
+
+    else:
+        total_energy += toy
+        total_toys += 1
+        elfs_energy.append(elf - toy + 1)
+        if day % 5 == 0:
+            total_toys -= 1
+            elfs_energy.append(elf - toy)
+            total_energy += toy
+print()
